4ae899d801889de1022a72bd5f2089c58f73d8de
Jonathan Ceasar Medina <120336249+JonathanCMedina@users.noreply.github.com>
2024-06-05 11:19:24 -0700

    Create goodbye.py (#2)

---
+++ goodbye.py
@@ -0,0 +1,4 @@
+print("Hello, it's me")
+print("I was wondering after all these years you'd like to meet")
+print("To go over everything")
+print("I've forgotten how it felt before the world fell at my feet")
